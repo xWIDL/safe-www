1788c853d2df3dfd2550fa33edd1d57d250d3b99
Zhen Zhang <izgzhen@gmail.com>
2017-01-11 10:33:38 +0800

    Upload and analyze

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
 var koa = require('koa');
+var logger = require('koa-logger');
+var route = require('koa-route');
+var parse = require('co-busboy');
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+var views = require('co-views');
 var app = koa();
+var execSync = require('child_process').execSync;
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
+var render = views(path.join(__dirname, '/views'), { ext: 'ejs' });
+
+app.use(logger());
+
+app.use(route.get('/', function *(){
+  this.body = 'safe-www';
+}));
+
+app.use(route.post('/submit', function *(next) {
+  // ignore non-POSTs
+  if ('POST' != this.method) return yield next;
+
+  // multipart upload
+  var parts = parse(this);
+  var part;
+
+  var responses = [];
+
+  while ((part = yield parts)) {
+    var filename = Math.random().toString() + ".js";
+    var filepath = path.join(os.tmpdir(), filename);
+    var buf = part.read();
+    fs.writeFileSync(filepath, buf);
+    var stdout = execSync('$SAFE_HOME/bin/safe analyze ' + filepath);
+    responses.push({
+        name: filename,
+        js: buf.toString(),
+        out: stdout.toString()
+    })
+  }
+
+  this.body = yield render('responses', { responses: responses });
+}));
 
 app.listen(3000);
+
