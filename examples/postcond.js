7171dcb5761859e1ab53ae1d66943c5618ec8b9d
Zhen Zhang <izgzhen@gmail.com>
2017-01-13 23:41:46 +0800

    Add new examples

---
+++ examples/postcond.js
@@ -0,0 +1,5 @@
+var foo = new Foo();
+
+var y = foo.one() + 4;
+
+foo.onlyFive(y); // this should be okay
---
+++ examples/state.js
@@ -0,0 +1,7 @@
+var foo = new Foo();
+
+foo.add(5);
+
+var y = foo.count; // y should be 5
+
+foo.onlyFive(y); // this should be okay
